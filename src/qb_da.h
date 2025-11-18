3d7688b73f6b81c2a52571efc35186a61b6ec220
xsoder <tadihailukebe@gmail.com>
2025-11-17 21:26:29 -0500

    feat: added license

--- src/qb_da.h
+++ src/qb_da.h
@@ -1,18 +1,3 @@
-/*
-The MIT License (MIT)
-Copyright © 2025 xsoder
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-/*
-For testing purpose when big migrate to nob.h by https://github.com/tsoding/nob.h. This is a helper header so you can have a genric way of writing any dynamic array. This is my attempt fo using a stripped down version of nob. This will also have features of bulding the project from scratch. I will fully make it a build system and have more functions implemented inside of it as well.
-*/
-
 #ifndef DA_H
 #define DA_H
 #include <stdlib.h>
